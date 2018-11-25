92010ef8aca4f25878672826edf1c74e68f473c8
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2018-11-24 20:08:41 -0600

    Se agrego página de registro de usuarios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 //Components
 import './App.css';
@@ -9,26 +9,36 @@ import UserHome from './containers/user-home/user-home'
 import UserSettings from './containers/user-settings/user-settings'
 import UserNotifications from './containers/user-notifications/user-notifications'
 import NavbarClient from './components/navbar-client'
-
+import RegisterForm from './containers/register-page/register-form'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      loggedIn: true,
+      loggedIn: false,
       username: null,
+      redirectTo: null,
     };
-
+    this.updateUser = this.updateUser.bind(this);
   }
+
+  updateUser(userObject) {
+    this.setState(userObject);
+  }
+
   render() {
 
     return (
       <div className="App">
-        <NavbarClient/>
-        <Route exact path="/" component={UserHome} />
-        <Route exact path="/ajustes" component={UserSettings} />
-        <Route exact path="/notificaciones" component={UserNotifications} />
-        <Route exact path="/login" component={LoginForm} />
+        <NavbarClient updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+        <Switch>
+          <Route path="/" exact render={() => <LoginForm updateUser={this.updateUser} />} />
+          <Route exact path="/ajustes" component={UserSettings} />
+          <Route exact path="/notificaciones" component={UserNotifications} />
+          <Route exact path="/login" component={LoginForm} />
+          <Route exact path="/register" component={RegisterForm} />
+        </Switch>
+
       </div>
     );
   }
