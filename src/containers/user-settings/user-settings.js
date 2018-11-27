98dc98ea8410d240578c6c382127fae6a17bbb58
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2018-11-27 09:24:16 -0600

    Login session

--- src/containers/user-settings/user-settings.js
+++ src/containers/user-settings/user-settings.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+
 import { Image } from 'react-bootstrap'
 import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 import image from '../../recursos/imagenes/profile.png'
 import "./user-settings.css"
-
+import { connect } from 'react-redux'
+import axios from 'axios'
 
 class UserSettings extends Component {
     constructor() {
@@ -18,11 +20,57 @@ class UserSettings extends Component {
                 password: "***********",
                 login: "10-09-2018",
                 edad: "22",
-                genero: "M"
+                genero: "M",
+                curp: ""
             }
         };
     }
 
+    componentDidMount() {
+        //Aqui mandamos a cargar el estado desde redux
+        console.log("hola")
+        this.cargarDatosUsuario()
+    }
+
+    cargarDatosUsuario(){
+        const datos = {
+            header : {
+              'Access-Control-Allow-Origin': '*',
+              'Content-Type': 'application/json'
+            }
+          }
+          
+        axios.get('http://el-equipo-perro.mybluemix.net/client/' + this.props.usr)
+            .then(response => {
+                console.log(response)
+                if (response.status === 200) {
+ 
+                    let nom = response.data.payload.nombres + ' ' + response.data.payload.apellidoMaterno + ' ' + response.data.payload.apellidoPaterno
+                    let ed = response.data.payload.edad
+                    let gen = response.data.payload.genero
+                    let cur = response.data.payload.curp
+                    let rf = response.data.payload.rfc
+
+                    this.setState({
+                        datosUs: {
+                            nombre: nom,
+                            permisos: "Administrador",
+                            password: "***********",
+                            login: "10-09-2018",
+                            edad: ed,
+                            genero: gen,
+                            curp: cur,
+                            rfc: rf   
+                        }
+                    })
+
+                    console.log(this.state)
+
+                }
+            }
+            
+        )}
+        
     render() {
         return (
             <div className="bloque">
@@ -35,10 +83,28 @@ class UserSettings extends Component {
                     <p>Password: {this.state.datosUs.password}</p>
                     <p>Edad: {this.state.datosUs.edad}</p>
                     <p>Genero: {this.state.datosUs.genero}</p>
+                    <p>Curp: {this.state.datosUs.curp}</p>
+                    <p>Rfc: {this.state.datosUs.rfc}</p>
                     <p>Last-login: {this.state.datosUs.login}</p>
                 </div>
             </div>
         );
     }
 }
-export default UserSettings;
+
+const mapStateToProps = state => {
+    return {
+      usr: state.user,
+      logged: state.logged,
+      usuarios: state.usuariosTodos
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      setUsuarios: (users) => dispatch({ type: 'SET_USUARIOS', usuariosTodos: users }),
+    };
+  };
+  
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserSettings);
